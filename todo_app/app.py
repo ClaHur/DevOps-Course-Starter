2af63901191a233f53e7df27b2d0a925a27aab11
ClaHur <clare.hurst@softwire.com>
2024-03-11 17:20:09 +0000

    Exercise 2 markups

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request, redirect
-from todo_app.data.session_items import add_item, get_items, update_status, get_lists
+from todo_app.data.session_items import TrelloService
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
-
+trello_service = TrelloService()
 
 @app.route('/')
 def index():
-    return render_template('index.html', items = get_items(), lists = get_lists())
+    return render_template('index.html', items = trello_service.get_items(), lists = trello_service.get_lists())
 
 @app.route("/add_item", methods=["POST"])
 def receive_form():
     form_value = request.form["todo"]
-    add_item(form_value)
+    trello_service.add_item(form_value)
     return redirect('/')
 
 @app.route("/update_status", methods=["POST"])
 def receive_update_form():
     item_id = request.json['itemId']
     is_checked = request.json['isChecked']
-    update_status(item_id, is_checked)
+    trello_service.update_status(item_id, is_checked)
     return redirect('/')
--- todo_app/data/session_items.py
+++ todo_app/data/session_items.py
@@ -12,97 +12,104 @@ class Item:
     def from_trello_card(cls, card, list):
         return cls(card['id'], card['name'], list['name'])
 
-board_id = os.getenv("TRELLO_BOARD_ID")
-api_key = os.getenv("TRELLO_API_KEY")
-token = os.getenv("TRELLO_TOKEN")
+class TrelloService():
 
-base_trello_url = "https://api.trello.com/1"
+    def __init__(self):
+        self.board_id = os.getenv('TRELLO_BOARD_ID')
+        self.api_key = os.getenv("TRELLO_API_KEY")
+        self.token = os.getenv("TRELLO_TOKEN")
+        self.base_trello_url = "https://api.trello.com/1"
 
-def get_lists():
-    """
-    Returns all the lists on the trello board
-    """
-    url = f"{base_trello_url}/boards/{board_id}/lists"
-    params = {
-        "key" : api_key,
-        "token" : token
-    }
+    def get_lists(self):
+        """
+        Returns all the lists on the trello board
+        """
+        url = f"{self.base_trello_url}/boards/{self.board_id}/lists"
+        params = {
+            "key" : self.api_key,
+            "token" : self.token
+        }
 
-    response = requests.get(url, params=params)
-    response_json = response.json()
+        response = requests.get(url, params=params)
+        response_json = response.json()
 
-    return response_json
+        return response_json
 
-def get_list(list_id):
-    """
-    Returns a list of a given ID
-    """
-    url = f"{base_trello_url}/list/{list_id}"
-    params = {
-        "key" : api_key,
-        "token" : token
-    }
+    def get_list(self, list_id):
+        """
+        Returns a list of a given ID
+        """
+        url = f"{self.base_trello_url}/list/{list_id}"
+        params = {
+            "key" : self.api_key,
+            "token" : self.token
+        }
 
-    response = requests.get(url, params=params)
-    response_json = response.json()
+        response = requests.get(url, params=params)
+        response_json = response.json()
 
-    return response_json
+        return response_json
 
-def get_items():
-    """
-    Fetches all items on the trello board
-    """
-    url = f"{base_trello_url}/boards/{board_id}/cards"
-    params = {
-        "key" : api_key,
-        "token" : token
-    }
+    def get_items(self):
+        """
+        Fetches all items on the trello board
+        """
+        url = f"{self.base_trello_url}/boards/{self.board_id}/cards"
+        params = {
+            "key" : self.api_key,
+            "token" : self.token
+        }
 
-    response = requests.get(url, params=params)
-    response_json = response.json()
+        response = requests.get(url, params=params)
+        response_json = response.json()
 
-    items = []
-    for card in response_json:
-        item = Item.from_trello_card(card, get_list(card["idList"]))
-        items.append(item)
+        list_refs = self.get_lists()
+        lists = {}
+        for list in list_refs:
+            lists[list['id']] = self.get_list(list['id'])
 
-    return items
+        items = []
+        for card in response_json:
+            item = Item.from_trello_card(card, lists[card["idList"]])
+            items.append(item)
 
-def get_item(id):
-    """
-    Fetches the item with the specified ID.
-    """
-    items = get_items()
-    return next((item for item in items if item['id'] == int(id)), None)
+        return items
+
+    def get_item(self, id):
+        """
+        Fetches the item with the specified ID.
+        """
+        items = self.get_items()
+        return next((item for item in items if item['id'] == int(id)), None)
 
 
-def add_item(title):
-    """
-    Adds a new item with the specified title to the list.
-    """
-    list_id = get_lists()[0]["id"]
-    url = f"{base_trello_url}/cards"
-    params = {
-        "idList": list_id,
-        "key" : api_key,
-        "token" : token,
-        "name" : title
-    }
+    def add_item(self, title):
+        """
+        Adds a new item with the specified title to the list.
+        """
+        list_id = self.get_lists()[0]["id"]
+        url = f"{self.base_trello_url}/cards"
+        params = {
+            "idList": list_id,
+            "key" : self.api_key,
+            "token" : self.token,
+            "name" : title
+        }
 
-    response = requests.post(url, params=params)
+        response = requests.post(url, params=params)
 
-    return response
+        return response
 
-def update_status(item_id, is_checked):
-    """
-    Updates the status of the item according to checkbox selection
-    """
-    new_list_id = get_lists()[1]["id"] if is_checked else get_lists()[0]["id"]
-    url = f"{base_trello_url}/cards/{item_id}"
-    params = {
-        "idList": new_list_id,
-        "key" : api_key,
-        "token" : token,
-    }
-    response = requests.put(url, params=params)
-    return response
+    def update_status(self, item_id, is_checked):
+        """
+        Updates the status of the item according to checkbox selection
+        """
+        new_list_id = self.get_lists()[1]["id"] if is_checked else self.get_lists()[0]["id"]
+        url = f"{self.base_trello_url}/cards/{item_id}"
+        params = {
+            "idList": new_list_id,
+            "key" : self.api_key,
+            "token" : self.token,
+        }
+        response = requests.put(url, params=params)
+        return response
